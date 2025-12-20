01be998de67add33155dc6ead1e046c10113e8f5
Abrar <abrar_2022bite057@nitsri.ac.in>
2025-12-20 13:39:54 +0530

    update

--- 2.ChatModels/5_1_chatmodel_hf_local.py
+++ 2.ChatModels/5_1_chatmodel_hf_local.py
@@ -1,17 +1,17 @@
 from langchain_huggingface import ChatHuggingFace,HuggingFacePipeline
 
 llm=HuggingFacePipeline.from_model_id(
-    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    model_id="HuggingFaceH4/zephyr-7b-beta",
     task='text-generation',
     pipeline_kwargs=dict(
         temperature=0.5,
-        max_new_tokens=100
+        max_new_tokens=1000
     )
 )
 
 
 model=ChatHuggingFace(llm=llm)
 
-result=model.invoke("What is capital of Zambia")
+result=model.invoke("Write a story about two students ,canteen, out of money, gpu, machinelearning,")
 
 print(result.content)
