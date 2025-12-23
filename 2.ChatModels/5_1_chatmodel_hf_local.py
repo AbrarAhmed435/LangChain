18568af44094e63e8f71f39945f4060283b62c38
Abrar <abrar_2022bite057@nitsri.ac.in>
2025-12-23 16:58:22 +0530

    update

--- 2.ChatModels/5_1_chatmodel_hf_local.py
+++ 2.ChatModels/5_1_chatmodel_hf_local.py
@@ -2,6 +2,7 @@ from langchain_huggingface import ChatHuggingFace,HuggingFacePipeline
 
 llm=HuggingFacePipeline.from_model_id(
     model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    #model_id="nvidia/Nemotron-Cascade-8B",
     task='text-generation',
     pipeline_kwargs=dict(
         temperature=0.5,
---
+++ 3.EmbedingModels/7_RAG_01.py
@@ -0,0 +1,106 @@
+
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from sklearn.metrics.pairwise import cosine_similarity
+from langchain_openai import ChatOpenAI
+import numpy as np
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from PyPDF2 import PdfReader
+
+import re
+
+def pdf_to_sentences(pdf_path):
+    reader=PdfReader(pdf_path)
+    text=""
+
+    for page in reader.pages:
+        text+=page.extract_text()+" "
+
+    text=text.replace("\n"," ")
+
+    sentences=re.split(r'(?<=[.!?])\s+',text)
+
+    sentences=[s.strip() for s in sentences if s.strip()]
+
+    return sentences
+
+SOURCE_PDF="/home/scratch-scholars/Abrar/LangChain/Documents/The_Cyclops_Story.pdf"
+
+document=pdf_to_sentences(SOURCE_PDF)
+
+print(document[:5])
+
+embedding=HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+from langchain_huggingface import ChatHuggingFace,HuggingFacePipeline
+
+llm=HuggingFacePipeline.from_model_id(
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task='text-generation',
+    pipeline_kwargs=dict(
+        temperature=0.5,
+        max_new_tokens=1000
+    )
+)
+model=ChatHuggingFace(llm=llm)
+
+model_2=ChatOpenAI(model='gpt-4o-mini',temperature=0.5)
+
+def generate_embeddings(document):
+    if isinstance(document,list):
+        return embedding.embed_documents(document)
+    else:
+        return embedding.embed_query(document)
+
+
+def calling_llm(question,answer):
+    print(f"Abrar==={answer}")
+    prompt=f'''
+you are an (Retrieval Augmented Generation) RAG assistant 
+RULES:
+- Answer ONLY using the provided context.
+- if you don't find context say i don't know
+- Do NOT use external knowledge.
+- Be concise and factual.
+context:{answer}
+question:{question}
+Give Answer
+'''
+    result=model.invoke(prompt)
+    return result.content
+
+
+doc_emb=generate_embeddings(document)
+
+
+
+question=""
+
+ques_emb=generate_embeddings(question)
+
+scores=cosine_similarity([ques_emb],doc_emb)[0]
+top_k=scores.argsort()[::-1][:4]
+
+Answers=[]
+
+for i in top_k:
+    Answers+=[document[i]]
+print(Answers)
+
+
+print("===LLM ANSWER===")
+
+print(calling_llm(question,Answers))
+
+
+
+
+
+
+
+
