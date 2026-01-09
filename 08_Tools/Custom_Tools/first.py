2f2cee9dbba244790df80051eff4b3841af9b279
Abrar ul riyaz <abrar_2022bite057@nitsri.ac.in>
2026-01-09 18:52:03 +0530

    update

---
+++ 08_Tools/Custom_Tools/first.py
@@ -0,0 +1,24 @@
+from langchain_core.tools import tool
+
+
+@tool
+def multiply(a:int,b:int)->int:
+    """ Multiply two numbers """
+    return a*b
+
+result=multiply.invoke({
+    "a":3,
+    "b":4
+})
+
+print(result)
+
+print(multiply.name)
+print(multiply.description)
+print(multiply.args)
+
+print()
+
+# LLM sees this 
+from pprint import pprint
+pprint(multiply.args_schema.model_json_schema())
